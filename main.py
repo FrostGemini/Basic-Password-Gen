eed389c435ce27a2f5e40a632c70b5b62596a780
Frost <58620132+FrostGemini@users.noreply.github.com>
2023-04-02 21:38:14 -0700

    Edit - 4.2.23
    Added additional generation options, number of generations, and character choice. Also added a minimum character count and default settings.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-
 import random
+import string
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-/?"
+def generate_password(length):
+    """
+    Generate a random password of a given length.
+    """
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for i in range(length))
+    return password
 
+def main():
+    while True:
+        try:
+            pass_len = int(input("How many characters? "))
+            if pass_len < 8:
+                raise ValueError("Password length must be at least 8 characters.")
+            pass_count = int(input("How many to generate? "))
+            if pass_count <= 0:
+                raise ValueError("Number of passwords to generate must be greater than 0.")
+            for i in range(pass_count):
+                password = generate_password(pass_len)
+                print(f"Password {i+1}: {password}")
+        except ValueError as e:
+            print(f"Error: {e}")
+        except KeyboardInterrupt:
+            print("\nExiting password generator.")
+            break
 
-while 1:
-    pass_len = int(input("How many characters? "))
-    pass_count = int(input("How many to generate? "))
-    for x in range(0,pass_count):
-        password = ""
-        for x in range(0,pass_len):
-            password_char = random.choice(chars)
-            password = password + password_char
-        print("Here's the password:", password)
+if __name__ == '__main__':
+    main()
